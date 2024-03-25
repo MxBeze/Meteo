5938623a96677c97df7295f692a6f486fd5d3cfb
MxBeze <MxBeze@users.noreply.github.com>
2024-03-25 16:27:28 -0600

    Created the Update weather and city  function

--- src/script.js
+++ src/script.js
@@ -1,14 +1,26 @@
+function updateWeather(response){
+let temperatureElement = document.querySelector("#weather-degree");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city){
+let apiKey = "312a98ffb2fdc23b3521b024dt925odb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
 function displayTemperature (event) {
 event.preventDefault();
 
 let searchInput = document.querySelector("#search-input");
 let cityElement = document.querySelector("#weather-city");
 cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value)
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", displayTemperature)
 
-let apiKey = "312a98ffb2fdc23b3521b024dt925odb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
