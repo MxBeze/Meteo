69b0c4774eff2fa13276d4013d507065d9636b9a
MxBeze <MxBeze@users.noreply.github.com>
2024-03-25 02:32:37 -0600

    Added basic Search Engine function

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function displayTemperature (event) {
+event.preventDefault();
+
+let searchInput = document.querySelector("#search-input");
+let cityElement = document.querySelector("#weather-city");
+cityElement.innerHTML = searchInput.value;
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", displayTemperature)
