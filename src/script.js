6d855029f9cbef240b3c8028c6eeb4e3dcc80b86
MxBeze <MxBeze@users.noreply.github.com>
2024-03-25 19:27:13 -0600

    Updated weather data.

--- src/script.js
+++ src/script.js
@@ -10,11 +10,31 @@ let humidityElement = document.querySelector("#humidity");
 
 let windElement = document.querySelector("#wind");
 
+let timeElement = document.querySelector("#time");
+
+let date = new Date (response.data.time * 1000);
+
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windElement.innerHTML = response.data.wind.speed;
+windElement.innerHTML = `${response.data.wind.speed}km/h`;
+timeElement.innerHTML = formatDate(date);
 temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date){
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
+
+let day = days[date.getDay()];
+
+if (minutes <10){
+minutes `0${minutes}`;
+}
+
+return `${day} ${hours}:${minutes}, `;
+
 }
 
 function searchCity(city){
